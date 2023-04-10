120130323ce134067179cf09ec3558222f78c4ca
unknown <icaroisl6582@gmail.com>
2023-04-10 09:43:31 -0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react'
 function App() {
+
+  const [primaryNumber, setPrimaryNumber] = useState()
+  const [SecondNumber, setSecondNumber] = useState()
+  const [inputNumber, setInputNumber] = useState()
+  const [mathOperation, setMathOperation] = useState("")
+
+
+  // COLOCAR O NUMERO NO INPUT QUANDO APERTAR O BOTÃO
+  const handleAddNumber = (e) => {
+
+      if(inputNumber === undefined) {
+        setInputNumber(e)
+      } else {
+        if(inputNumber.length <= 4) {
+          setInputNumber(inputNumber + e)
+        }
+        
+      }
+
+  }
+
+  const handleCalculate = (e) => {
+
+    setPrimaryNumber(parseInt(inputNumber))
+      
+    setInputNumber()
+    setMathOperation(e)
+    
+  }
+  const handleResult = () => {
+
+    setSecondNumber(parseInt(inputNumber))
+
+  }
+
+  useEffect(() => {
+    if(mathOperation === "division") {
+      setInputNumber(primaryNumber / SecondNumber)
+    }
+    if(mathOperation === "multiplication") {
+      setInputNumber(primaryNumber * SecondNumber)
+    }
+    if(mathOperation === "addition") {
+      setInputNumber(primaryNumber + SecondNumber)
+    }
+    if(mathOperation === "subtraction") {
+      setInputNumber(primaryNumber - SecondNumber)
+    }
+  }, [SecondNumber])
+
+  const handleClear = () => {
+    setInputNumber()
+    setPrimaryNumber()
+    setSecondNumber()
+    setMathOperation()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Calculator">
+        <div className="Result-Content">{inputNumber? inputNumber : 0}</div>
+        <div className="Buttons">
+          <div>
+            <button onClick={() => handleAddNumber("7")}>7</button>
+            <button onClick={() => handleAddNumber("8")}>8</button>
+            <button onClick={() => handleAddNumber("9")}>9</button>
+            <button onClick={() => handleCalculate("division")}>%</button>
+          </div>
+          <div>
+            <button onClick={() => handleAddNumber("4")}>4</button>
+            <button onClick={() => handleAddNumber("5")}>5</button>
+            <button onClick={() => handleAddNumber("6")}>6</button>
+            <button onClick={() => {handleCalculate("multiplication"); handleCalculate("multiplication");}}>*</button>
+          </div>
+          <div>
+            <button onClick={() => handleAddNumber("3")}>3</button>
+            <button onClick={() => handleAddNumber("2")}>2</button>
+            <button onClick={() => handleAddNumber("1")}>1</button>
+            <button onClick={() => handleCalculate("addition")}>+</button>
+          </div>
+          <div>
+            <button onClick={() => handleAddNumber("0")}>0</button>
+            <button onClick={() => handleClear()}>C</button>
+            <button onClick={() => handleResult()}>=</button>
+            <button onClick={() => handleCalculate("subtraction")}>-</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
